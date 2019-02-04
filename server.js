0b9803672c860040a0a1dfe647a0a5d53fba6366
Lucas <lucashsmoraes@gmail.com>
2019-02-04 21:44:29 -0200

    testando nodemon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello word!')
+    res.send('Hello Teste nodemon!')
 });
 
 app.listen(3001);
