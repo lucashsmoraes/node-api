ee3589d464ed969eef28cdc3c78cfe4579b1896a
Lucas <lucashsmoraes@gmail.com>
2019-02-09 21:36:31 -0200

    criando metodo post

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const requireDir = require('require-dir')
 
 // Iniciando o App
 const app = express();
+// permiti enviar dados no formato json
+app.use(express.json())
 
 //Iniciando o DB
-// mongoose.connect("mongodb://localhost:27017/nodeapi", {
-//   useNewUrlParser: true
-// });
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true
+});
 requireDir("./src/models");
 
 // Rotas
