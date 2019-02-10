975ae42d56c2ee49345c8b0c0eec2b88715fdd47
Lucas <lucashsmoraes@gmail.com>
2019-02-09 22:48:40 -0200

    adicionando cors na aplicacao

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 // Iniciando o App
 const app = express();
 // permiti enviar dados no formato json
 app.use(express.json())
+app.use(cors())
 
 //Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
